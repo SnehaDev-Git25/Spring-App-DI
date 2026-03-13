5ad58f65bab003d2bee431e6a3f41e3e170ea9c5
SnehaDev-Git25 <ghodesneha96@gmail.com>
2026-03-13 18:27:05 +0530

    Jira Task 102 completed

--- src/main/java/in/ashokit/Printer.java
+++ src/main/java/in/ashokit/Printer.java
@@ -5,9 +5,21 @@ public class Printer {
 	public Printer() {
 		System.out.println("Printer :: 0-Param Constructor");
 	}
+	//Jira task:102
+	public String showColorPrint(){
+		System.out.println("Coloured copies");
 	
+	}
+
+
+
 	public void print() {
 		System.out.println("Printing Reciept of the money history....");
+	}
+	//Jira task: 101
+	public void takingCopies(){
+	      System.out.println("Taking the copies to perform priniting");
+
 	}
 	
 	
